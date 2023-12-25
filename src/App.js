bc58b676dd8ac84f5bb091208532f08f557cae88
Pedro Lameira <120721002+lameiragaspar@users.noreply.github.com>
2023-12-25 10:58:28 +0100

    On-Off
    Como criar um app com o dark mode ativado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  {/*Classes*/}
+  {/*1 - Estatos*/}
+  const On = 'ativo'
+  const Off = 'inativo'
+  const [estatos, setEstatos] = useState(true)
+  {/*2 - mode*/}
+  const dark = 'dark'
+  const white = 'white'
+  const [mode, setMode] = useState(true)
+
+  {/*textos*/}
+  const text_on = 'ON'
+  const text_of = 'OFF'
+  const [novoTexto, setNovoTexto] = useState(true)
+  
+  function alteraEstatos(){
+    setEstatos(!estatos)
+    setNovoTexto(!novoTexto)
+    setMode(!mode)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={mode? white : dark}>
+      <div className='container'>
+        <div onClick={alteraEstatos} className={estatos? On : Off}>
+          <button onClick={alteraEstatos}  >
+            {novoTexto ? text_on : text_of}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
